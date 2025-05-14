04db7fabd365d96560383d7d42df0813864dbc27
Guillaume Gorvel <ggorvel@gmail.com>
2025-05-14 23:18:54 +0200

    add login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,16 +1,29 @@
 describe('login', () => {
-  it('passes', () => {
+  it('inscrit puis connecte un utilisateur', () => {
     cy.visit('http://localhost:5173');
+
     cy.get('#show-register').click();
-    cy.get('#register-username').type('test');
-    cy.get('#register-email').type('test@test.fr')
-    cy.get('#register-password').type('qwerty');
+
+    const email = `test+${Date.now()}@test.fr`;
+    const password = 'qwerty';
+    const username = 'test';
+
+    cy.get('#register-username').type(username);
+    cy.get('#register-email').type(email);
+    cy.get('#register-password').type(password);
     cy.get('#register-btn').click();
-    cy.wait(5000);
-    cy.get('#login-email').type('test@test.fr');
-    cy.get('#login-password').type('qwerty');
+
+    cy.wait(1000);
+
+    // Connexion
+    cy.get('#login-email').type(email);
+    cy.get('#login-password').type(password);
     cy.get('#login-form > .login-btn').click();
 
-    cy.get('#score').contains('Score');
-  })
-})
+    cy.get('#score').should('exist');
+
+    cy.wait(1000);
+    cy.get('.logout').click();
+    cy.get('#login-container > h2').contains('Se connecter');
+  });
+});
